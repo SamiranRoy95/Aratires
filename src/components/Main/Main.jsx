a3ff6184402efb59d68f5535d126864305f6a2c2
SamiranRoy95 <somironroy80@gmail.com>
2024-03-05 20:35:54 +0600

    update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 
+
+const d=[
+  { name:"aaa"},
+  {name:"ddddd"}
+]
 const Main = () => {
+  const [post, setPost] = useState()
+
+
+  useEffect(() => {
+
+    fetch("./data.json")
+      .then(res => res.json())
+      .then(data => {
+        setPost(data)
+        console.log(data)
+      })
+  }, [])
   return (
-    <div>Main</div>
+    <div >
+      <div>
+        {
+          post.map((p) => (
+            <div >
+
+              <img src={p.image} />
+              <h2>{p.name}</h2>
+            </div>
+
+          ))
+        }
+      </div>
+
+    </div>
   )
 }
 
